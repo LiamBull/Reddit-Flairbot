ed2043a6a660f429e88e9ca8240576ffd867a596
Liam <lphilipbull@gmail.com>
2017-08-14 23:01:21 -0400

    Improved documentation

--- bot.py
+++ bot.py
@@ -1,28 +1,38 @@
-import praw
-import datetime
-import config
+"""This program checks new posts to a specified subreddit, and if they are not flaired
+within 18-20min, the bot will send a private message to the user. This message as well
+as it's title and the subreddit the bot is intended to be run on can be specified in
+the config.py file."""
+
 import time
+import datetime
+import praw
+import config
 
 def bot_login():
-    reddit = praw.Reddit(username = config.username,
-            password = config.password,
-            client_id = config.client_id,
-            client_secret = config.client_secret,
+    """This function logs the bot into reddit given the parameters specified in the config.py"""
+    reddit = praw.Reddit(username = config.USERNAME,
+            password = config.PASSWORD,
+            client_id = config.CLIENT_ID,
+            client_secret = config.CLIENT_SECRET,
             user_agent = "Flair Bot")
-    
+
     return reddit
-    
+
 def run_bot(reddit):
-    for submission in reddit.subreddit(config.subreddit).new(limit=20):
+    """Runs the bot, checking new posts in a subreddit and messaging users who do not flair their
+    posts within the specified timeframe"""
+    for submission in reddit.subreddit(config.SUBREDDIT).new(limit=20):
         posttime = get_dif(submission)
-        if posttime > datetime.timedelta(minutes=18) and posttime < datetime.timedelta(minutes=20) and submission.link_flair_text is None:
-            submission.author.message(config.title, config.message)
+        if (posttime > datetime.timedelta(minutes=18) and posttime < datetime.timedelta(minutes=20)
+                and submission.link_flair_text is None):
+            submission.author.message(config.TITLE, config.MESSAGE)
             print submission.author
-        
+
 def get_dif(submission):
+    """This function gets the time difference between the time the thread was updated and now"""
     date = datetime.datetime.fromtimestamp(submission.created_utc)
     dif = datetime.datetime.utcnow() - date
-    
+
     return dif - datetime.timedelta(hours=4)
 
 reddit = bot_login()
--- config.py
+++ config.py
@@ -1,10 +1,16 @@
-username = ""
-password = ""
-client_id = ""
-client_secret = ""
+"""This is the config file for a flair bot created for use on reddit.
+Within this file you will specify the bot's account information, as well as
+information regarding the bot's output and run location."""
+
+#The username and password of the bot's reddit account.
+USERNAME = ""
+PASSWORD = ""
+#The codes associated with the bot, provided through the Reddit API.
+CLIENT_ID = ""
+CLIENT_SECRET = ""
 #Title should be the title of the message to the user
-title = ""
+TITLE = ""
 #Message should be the body of the message to the user
-message = ""
+MESSAGE = ""
 #The subreddit you wish to run it on
-subreddit = ""
+SUBREDDIT = ""
